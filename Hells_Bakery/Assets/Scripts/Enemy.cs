18263e51bf76adb2e3d2d360218c2e6056e12523
samwgitt <113568930+samwgitt@users.noreply.github.com>
2022-10-19 19:32:42 -0500

    Added basic AI movement

--- Hells_Bakery/Assets/Scripts/Enemy.cs
+++ Hells_Bakery/Assets/Scripts/Enemy.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float damage;
+    public Transform player;
+    public NavMeshAgent enemy;
+
+    void Update() {
+        enemy.SetDestination(player.position);
+    }
 
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player") {
