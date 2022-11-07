b459f4f5b43a81477d9840526695ef48e651a627
samwgitt <113568930+samwgitt@users.noreply.github.com>
2022-11-07 12:38:14 -0600

    Added a score counter

--- Hells_Bakery/Assets/Scripts/Player/PlayerMovement.cs
+++ Hells_Bakery/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     //public Animator animator;
     Vector3 movement;
+    public float score;
+    public TextMeshProUGUI scoreText;
 
 
     // Update is called once per frame
@@ -15,6 +18,8 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.z = Input.GetAxisRaw("Vertical");
+        score = score + 1;
+        scoreText.text = "Score: " + score.ToString();
 
         //animator.SetFloat("Horizontal", movement.x);
         //animator.SetFloat("Vertical", movement.z);
